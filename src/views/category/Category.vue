9073c05add6a185447a6c38a525831cfaa89ea19
shmily-cmd <yul8888@126.com>
2021-06-26 22:32:18 +0800

    第一次提交

---
+++ src/views/category/Category.vue
@@ -0,0 +1,26 @@
+<template>
+<h2>分类</h2>
+</template>
+
+<script>
+
+  export default {
+    name: "Category",
+    data() {
+      return {
+        
+      }
+    },
+    // 组件创建完后调用
+    mounted() {
+   
+    },
+    methods: {
+      
+    }
+  }
+</script>
+
+<style scoped>
+ 
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+  </div>
+</template>
+
+<script>
+  import NavBar from 'components/common/navbar/NavBar.vue'
+  import HomeSwiper from './childComps/HomeSwiper.vue'
+  import {getHomeMultidata} from "network/home"
+  import RecommendView from './childComps/RecommendView.vue'
+  
+  export default {
+    name: "Home",
+    components: {
+      NavBar,
+      HomeSwiper,
+      RecommendView
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: []
+      }
+    },
+    created() {
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      })
+    },
+  }
+</script>
+
+<style scoped>
+  .home-nav {
+    background-color: var(--color-tint);
+    color: white;
+  }
+</style>
